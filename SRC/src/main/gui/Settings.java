3010cb681eb3d8132a7fb6934535ec9dc2f5e8db
ishitaa20 <99643685+ishitaa20@users.noreply.github.com>
2022-11-10 13:51:11 +0530

    Complete project

---
+++ SRC/src/main/gui/Settings.java
@@ -0,0 +1,31 @@
+package main.gui;
+
+import main.game.Player;
+
+public class Settings{
+
+    public static Colour AIcolour = Colour.BLACK; // Note: starting player gets white pieces
+    public static boolean helpMode = true;
+    public static boolean hintMode = false;
+    public static boolean dragDrop = false;
+    public static int AiMinPauseDurationInMs = 800;
+    public static int squareSize = 80;
+    public static int checkerWidth = 5*squareSize/6;
+    public static int checkerHeight = 5*squareSize/6;
+    public static int ghostButtonWidth = 30*squareSize/29;
+    public static int ghostButtonHeight = 5*squareSize/6;
+
+    public static Colour getColour(Player player){
+        Colour result = null;
+        if (player == Player.AI){
+            result = Settings.AIcolour;
+        }
+        else if (player == Player.HUMAN){
+            result = Settings.AIcolour.getOpposite();
+        }
+        if(result == null){
+            throw new RuntimeException("Null player has no piece.");
+        }
+        return result;
+    }
+}
---
+++ SRC/src/main/gui/SquarePanel.java
@@ -0,0 +1,35 @@
+package main.gui;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Black or white square panel on checkerboard
+ */
+public class SquarePanel extends JPanel {
+
+    private Color color;
+
+    public SquarePanel(int i, int j){
+        this.setPreferredSize(new Dimension(Settings.squareSize,Settings.squareSize));
+        if( ((i % 2) + (j % 2)) % 2 == 0){
+            color = Color.WHITE;
+        }
+        else{
+            color = Color.BLACK;
+        }
+    }
+
+    public void setHighlighted(){
+        color = Color.GRAY;
+    }
+
+
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(color);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+}
