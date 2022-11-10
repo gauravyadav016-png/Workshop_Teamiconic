9580780a65d1b10042f256396b0784a497197a32
Harsh Anurag <87922734+harshsinha2301@users.noreply.github.com>
2022-11-10 13:41:34 +0530

    Frontend

---
+++ SRC/src/main/gui/Colour.java
@@ -0,0 +1,21 @@
+package main.gui;
+
+
+public enum Colour {
+    WHITE,
+    BLACK;
+
+    public Colour getOpposite(){
+        Colour result = null;
+        if (this == WHITE){
+            result = BLACK;
+        }
+        else if (this == BLACK){
+            result = WHITE;
+        }
+        if(result == null){
+            throw new RuntimeException("Null piece has no opposite");
+        }
+        return result;
+    }
+}
